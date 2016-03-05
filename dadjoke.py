284cfcf161929a6f512b7f6693fd3dfa81a15a24
Andrew Heekin <andrewheekin@gmail.com>
2016-03-05 11:09:50 -0500

    routes

--- dadjoke.py
+++ dadjoke.py
@@ -31,13 +31,13 @@ class Message(db.Model):
 db.create_all()
 
 
-@app.route('/', methods=['GET'])
+@app.route('/joke', methods=['GET'])
 def index():
     return render_template('index.html',
             messages = Message.query.order_by(Message.pub_date.desc()).all()
         )
 
-@app.route('/joke', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def dadjoke_ready():
     txt = request.values.get('Body').lower()
     dad_joke = ['dad joke', 'dadjoke', 'dad-joke']

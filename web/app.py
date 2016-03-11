98bdf0f3705a52e56c48bacee5981d915e46d90f
Andrew Heekin <andrewheekin@gmail.com>
2016-03-11 16:49:05 -0500

    added redis set to prevent telling same joke twice

--- web/app.py
+++ web/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import BaseConfig
 
+
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
@@ -14,35 +15,43 @@ redis = Redis(host='redis', port=6379)
 with open('jokes.json') as json_data_file:
     jokes = json.load(json_data_file)
 
+
 from models import *
+
 
 @app.route('/', methods=['GET'])
 def index():
+    redis.incr('pageViews')
     messages = Message.query.order_by(Message.date_posted.desc()).all()
-    return render_template('index.html', messages=messages)
+    return render_template('index.html', messages=messages, pageviews=int(redis.get('pageViews')))
 
 @app.route('/joke', methods=['POST'])
 def dadjoke_ready():
-    redis.incr('jokesTold')
-    print(redis.get('jokesTold'))
     txt = request.values.get('Body')
     dad_joke = ['dad joke', 'dadjoke', 'dad-joke']
     #if txt is received asking for a dad joke
     if any(x in txt.lower() for x in dad_joke):
         senderNum = request.values.get('From')
-
         resp = twilio.twiml.Response()
         resp.sms(newJoke(senderNum))
-
         message = Message(request.values.get('MessageSid'), senderNum, txt)
         db.session.add(message)
         db.session.commit()
-
         return str(resp)
 
 def newJoke(phoneNum):
-    joke = random.choice(jokes)
-    return joke
+    # create a new set [0, 1...len(jokes)] if not already exists
+    if (redis.exists(phoneNum)==False):
+        redis.sadd(phoneNum, *range(len(jokes)))
+    # return a random joke number then remove it
+    rand = redis.spop(phoneNum)
+    print('Phone number: ', phoneNum, ', ', 'popped: ',
+        rand, ', ', 'joke set: ', redis.smembers(phoneNum))
+    # convert string to int
+    rand = int(rand)
+    # track the timestamps
+    # redis.set(phoneNum+'_timeTold')
+    return jokes[rand]
 
 
 if __name__ == '__main__':

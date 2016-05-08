2299dd38c54013d11373587e267d9b8f89bd071f
Andrew Heekin <andrewheekin@gmail.com>
2016-05-07 20:16:12 -0400

    \n for sms linebreak

--- web/app.py
+++ web/app.py
@@ -53,7 +53,7 @@ def newJoke(phoneNum):
     redis.sadd(phoneNum+'_timestamp', [rand, datetime.datetime.now()-timedelta(hours=5)])    
     rand = int(rand)
     # %0a indicates line break
-    return jokes[rand] + '%0a%0a' + '👔http://dadjokebot.com'
+    return jokes[rand] + '\n\n👔http://dadjokebot.com'
 
 @app.route('/pageviews', methods=['GET'])
 def getPageviews():
